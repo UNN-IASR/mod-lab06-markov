167a3852b416629321ab779157c3be0a546050d7
NIKITA Vorobyev <73196137+neketskk@users.noreply.github.com>
2022-05-23 23:05:51 +0300

    Update textgen.h

--- include/textgen.h
+++ include/textgen.h
@@ -7,15 +7,15 @@
 
 class PROG {
  private:
-    string data;
-    vector<string> words;
-    typedef deque<string> prefix;
-    map<prefix, vector<string> > statetab;
+    std::string data;
+    std::vector<std::string> words;
+    typedef std::deque<std::string> prefix;
+    std::map<prefix, std::vector<std::string> > statetab;
     int NPREF;
     int MAXPROG;
  public:
-    PROG(string arr, int val1, int val2);
-    string getText();
-    PROG(map<prefix, vector<string> > val, vector<string> words,
+    PROG(std::string arr, int val1, int val2);
+    std::string getText();
+    PROG(map<prefix, std::vector<std::string> > val, std::vector<std::string> words,
     int val1, int val2);
 };
