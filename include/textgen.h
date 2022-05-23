5b06d993eef7d4587a3378e016c94d4450d7da1c
NIKITA Vorobyev <73196137+neketskk@users.noreply.github.com>
2022-05-23 23:07:45 +0300

    Update textgen.h

--- include/textgen.h
+++ include/textgen.h
@@ -16,6 +16,7 @@ class PROG {
  public:
     PROG(std::string arr, int val1, int val2);
     std::string getText();
-    PROG(map<prefix, std::vector<std::string> > val, std::vector<std::string> words,
+    PROG(map<prefix, std::vector<std::string> > val,
+    std::vector<std::string> words,
     int val1, int val2);
 };
