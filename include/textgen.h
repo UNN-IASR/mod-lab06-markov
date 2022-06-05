95080ed67a6f4318579f88646b63f4c86ae69198
kseniyaa3 <98958076+kseniyaa3@users.noreply.github.com>
2022-06-05 20:32:17 +0300

    Update textgen.h

--- include/textgen.h
+++ include/textgen.h
@@ -8,12 +8,14 @@
 #include <fstream>
 
 class Generator {
+    
 private:
     const int NPREF = 2;
     const int MAXGEN = 1000;
     typedef std::deque<std::string> prefix;
     std::map<prefix, std::vector<std::string> > statetab;
     std::vector<std::string> text;
+    
 public:
     Generator(std::string path);
     std::string newText();
