4393f0bd66c3ec6ddd67b1acf0dd0e30c9864250
prosto-vorobey <100966808+prosto-vorobey@users.noreply.github.com>
2022-06-05 19:30:07 +0300

    Update textgen.cpp

--- src/textgen.cpp
+++ src/textgen.cpp
@@ -24,18 +24,20 @@ void TextGenerator::readFile() {
 
     int i = 0;
     int j = 0;
-    int n = -1;
+    int n = 0;
 
     while (textfile[i] != '\0') {
         if (j == 0 && textfile[i] != ' ') {
             j = 1;
-            n++;
             words.push_back("");
             words[n] = words[n] + textfile[i];
-        } else if (j == 1 && (textfile[i] == ' ')) {
+            n++;
+        }
+        else if (j == 1 && (textfile[i] == ' ')) {
             j = 0;
-        } else if (j == 1 && textfile[i] != ' ') {
-            words[n] = words[n] + textfile[i];
+        }
+        else if (j == 1 && textfile[i] != ' ') {
+            words[n - 1] = words[n - 1] + textfile[i];
         }
         i++;
     }
@@ -47,7 +49,8 @@ void TextGenerator::readFile() {
         }
         if (i + NPREF < words.size()) {
             stateTab[pref].push_back(words[i + NPREF]);
-        } else {
+        }
+        else {
             stateTab[pref].push_back("end_file");
         }
     }
@@ -56,7 +59,7 @@ void TextGenerator::readFile() {
 std::string TextGenerator::generate() {
     setlocale(LC_ALL, "Russian");
     readFile();
-    srand(4561);
+
     prefix pref;
     std::string text = "";
     int n = 1;
@@ -69,29 +72,22 @@ std::string TextGenerator::generate() {
 
     while (text.size() < MAXGEN) {
         int veroyatnost;
-
         if (stateTab[pref].size() == 1 && stateTab[pref][0] == "end_file") {
             break;
         }
-
         if (stateTab[pref].size() == 0) {
             break;
         }
-
         veroyatnost = random() % stateTab[pref].size();
-
         if (stateTab[pref][stateTab[pref].size() - 1] == "end_file") {
             veroyatnost = random() % (stateTab[pref].size() - 1);
         }
-
         text = text + stateTab[pref][veroyatnost] + ' ';
-
-        std::string a = stateTab[pref][veroyatnost];
+        std::string again = stateTab[pref][veroyatnost];
         for (int i = 0; i < NPREF - 1; i++) {
             pref[i] = pref[i + 1];
         }
-        pref[NPREF - 1] = a;
-
+        pref[NPREF - 1] = again;
         if (n == 15) {
             text = text + '\n';
             n = 1;
