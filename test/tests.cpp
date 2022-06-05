bf3f6603f3d046f8b9c256effeabea55e5142a33
prosto-vorobey <100966808+prosto-vorobey@users.noreply.github.com>
2022-06-05 18:42:29 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,9 +1,9 @@
-// Copyright 2022 UNN-IASR
+// Copyright 2021 GHA Test Team
 #include <gtest/gtest.h>
 #include "textgen.h"
 
 TEST(task1, test1) {
     TextGenerator gentext = TextGenerator("test1.txt", 2, 1000);
-    gentext.readFile();
+    gentext.generate();
     ASSERT_EQ(generator.words.size(), 6);
 }
