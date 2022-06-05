ef3dc75137b0c797c29472256f0287137c611326
kseniyaa3 <98958076+kseniyaa3@users.noreply.github.com>
2022-06-05 20:44:20 +0300

    Update textgen.h

--- include/textgen.h
+++ include/textgen.h
@@ -10,13 +10,13 @@
 #define rand_r rand
 
 class Generator { 
-private:
+ private:
     const int NPREF = 2;
     const int MAXGEN = 1000;
     typedef std::deque<std::string> prefix;
     std::map<prefix, std::vector<std::string> > statetab;
     std::vector<std::string> text;
-public:
+ public:
     explicit Generator(std::string path);
     std::string newText();
     std::string Suffix(std::deque<std::string> prefdeq);
