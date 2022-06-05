ab0209ffb7da0f90afc80b60f651a5f42e1bb441
prosto-vorobey <100966808+prosto-vorobey@users.noreply.github.com>
2022-06-05 18:15:52 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,2 +1,14 @@
-// Copyright 2021 GHA Test Team
+// Copyright 2022 UNN-IASR
 #include <gtest/gtest.h>
+#include "textgen.h"
+
+TEST(task1, test1) {
+    std::string text = "test1.txt";
+    TextGenerator gentext = TextGenerator(text, 2, 1000);
+    typedef std::deque<std::string> prefix;
+    prefix pre;
+    pre.push_back("test");
+    pre.push_back("will");
+    std::string result = gentext.stateTab[pre][0];
+    EXPECT_EQ("be", result);
+}
