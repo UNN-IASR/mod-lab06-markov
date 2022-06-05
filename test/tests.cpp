cb6e2c4832daeecbe830f60ac106df91a7a683d0
prosto-vorobey <100966808+prosto-vorobey@users.noreply.github.com>
2022-06-05 18:46:18 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -5,5 +5,5 @@
 TEST(task1, test1) {
     TextGenerator gentext = TextGenerator("test1.txt", 2, 1000);
     gentext.generate();
-    ASSERT_EQ(generator.words.size(), 6);
+    ASSERT_EQ(gentext.words.size(), 6);
 }
