2ba435c9c89c77190e0a78b8c3ff9282c99b302e
prosto-vorobey <100966808+prosto-vorobey@users.noreply.github.com>
2022-06-05 19:25:53 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,13 +1,9 @@
 // Copyright 2021 GHA Test Team
 #include <gtest/gtest.h>
-#include <sstream>
-#include <string>
 #include "textgen.h"
 
 TEST(task1, test1) {
-    std::string text = "test1.txt";
-    TextGenerator gentext = TextGenerator(text, 2, 1000);
+    TextGenerator gentext = TextGenerator("test1.txt", 2, 1000);
     gentext.generate();
-    int result = gentext.words.size();
-    EXPECT_EQ(6, result);
+    ASSERT_EQ(gentext.words.size(), 6);
 }
