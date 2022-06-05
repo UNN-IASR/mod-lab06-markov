f261e176165ae79df6ae74ddd5db7f89351ae13b
prosto-vorobey <100966808+prosto-vorobey@users.noreply.github.com>
2022-06-05 19:31:17 +0300

    Update textgen.cpp

--- src/textgen.cpp
+++ src/textgen.cpp
@@ -32,11 +32,9 @@ void TextGenerator::readFile() {
             words.push_back("");
             words[n] = words[n] + textfile[i];
             n++;
-        }
-        else if (j == 1 && (textfile[i] == ' ')) {
+        } else if (j == 1 && (textfile[i] == ' ')) {
             j = 0;
-        }
-        else if (j == 1 && textfile[i] != ' ') {
+        } else if (j == 1 && textfile[i] != ' ') {
             words[n - 1] = words[n - 1] + textfile[i];
         }
         i++;
@@ -49,8 +47,7 @@ void TextGenerator::readFile() {
         }
         if (i + NPREF < words.size()) {
             stateTab[pref].push_back(words[i + NPREF]);
-        }
-        else {
+        } else {
             stateTab[pref].push_back("end_file");
         }
     }
