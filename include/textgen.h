e5d4f2fbb87b54a247ac95c7fe40bf90f3802808
kseniyaa3 <98958076+kseniyaa3@users.noreply.github.com>
2022-06-05 20:28:52 +0300

    Create textgen.h

---
+++ include/textgen.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <map>
+#include <fstream>
+
+class Generator {
+private:
+    const int NPREF = 2;
+    const int MAXGEN = 1000;
+    typedef std::deque<std::string> prefix;
+    std::map<prefix, std::vector<std::string> > statetab;
+    std::vector<std::string> text;
+public:
+    Generator(std::string path);
+    std::string newText();
+    std::string Suffix(std::deque<std::string> prefdeq);
+};
