5293638d7329d3c12427dc608f9aa4c53ece886a
Roma Golubev <romag643@gmail.com>
2025-04-27 17:13:16 +0300

    pupa3

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,16 @@
+// Copyright 1901 Romanchik333
+
+#include <ctime>
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <ctime>
 
 #include "textgen.h"
 
 int main() {
     std::ifstream input("../../datasets/checkov.txt");
     TextGenerator gena(input);
-    //gena.PrintTable(std::cout);
+    // gena.PrintTable(std::cout);
     std::cout << gena.Generate(100, time(0));
     std::ofstream output("../../result/result.txt");
     output << gena.Generate(1010, time(0));
