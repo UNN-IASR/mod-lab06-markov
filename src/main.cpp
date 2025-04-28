3cd1e16f5716e5fbdcca86c41013fa3e59c2664d
Timeix3 <timeix3@gmail.com>
2025-04-28 22:17:51 +0300

    lab 6 is complete

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+// Copyright 2022 UNN-IASR
+#include <fstream>
+#include "textgen.h"
+
+int main()
+{
+	const int NPREF = 2;
+	const int MAXGEN = 1000;
+	TextGenerator textgen = TextGenerator(NPREF, MAXGEN);
+	textgen.gatherData("../../src/chekhov.txt");
+	std::ofstream outFile("../../result/gen.txt");
+	textgen.generateText(outFile);
+	outFile.close();
+}
