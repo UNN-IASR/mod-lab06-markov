303fe8838ee1ac678ff1e64f3af6b91549e7e149
Sasha <kkkkkkp_16@mail.ru>
2025-04-30 20:33:15 +0300

    add solution of task1 text generator markov chains

---
+++ include/textgen.h
@@ -0,0 +1,26 @@
+#ifndef TEXTGEN_H
+#define TEXTGEN_H
+
+#include <deque>
+#include <map>
+#include <vector>
+#include <string>
+
+using Prefix = std::deque<std::string>;
+
+using StateTable = std::map<Prefix, std::vector<std::string>>;
+
+class TextGenerator {
+private:
+    StateTable table;
+    Prefix currentPrefix;
+    int prefixLength;
+    std::vector<Prefix> allPrefixes;
+
+public:
+    TextGenerator(int length = 2);
+    void learnFromText(const std::string& filename);
+    void generateText(int wordCount, const std::string& outputFile);
+};
+
+#endif
