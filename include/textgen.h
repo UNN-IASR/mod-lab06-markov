b1bfd4cf361000aa317889de1d63e03ffcc9dfb3
Sasha <kkkkkkp_16@mail.ru>
2025-04-30 21:09:06 +0300

    add tests

--- include/textgen.h
+++ include/textgen.h
@@ -11,16 +11,22 @@ using Prefix = std::deque<std::string>;
 using StateTable = std::map<Prefix, std::vector<std::string>>;
 
 class TextGenerator {
-private:
+ private:
     StateTable table;
     Prefix currentPrefix;
     int prefixLength;
     std::vector<Prefix> allPrefixes;
 
-public:
+ public:
     TextGenerator(int length = 2);
     void learnFromText(const std::string& filename);
     void generateText(int wordCount, const std::string& outputFile);
+    std::vector<Prefix> GetAllPrefixes() {
+        return allPrefixes;
+    }
+    StateTable GetTable() {
+        return table;
+    }
 };
 
 #endif
