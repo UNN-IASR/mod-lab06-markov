782b5a4a9a5259631cf495f7a2ebdb2da7293e96
Tulen4ick <zaharsazanov2015@gmail.com>
2025-04-30 14:03:00 +0300

    attempt to correct the design 2

--- include/textgen.h
+++ include/textgen.h
@@ -16,21 +16,21 @@ const int MAXGEN = 1000;
 typedef std::deque<std::string> prefix;
 
 class TextGeneratorClass {
-public: 
-    explicit TextGeneratorClass(std::string Filepath);
-    std::string generateText();
-    std::map < prefix, std::vector<std::string>> buildStateTab
-    (std::vector<std::string> words);
-    prefix GetStartPrefix();
-    void SetStartPrefix(prefix newStartPrefix);
-    std::map < prefix, std::vector<std::string>> GetStateTab();
-    void SetStateTab(std::map < prefix, std::vector<std::string>> newStateTab);
-    std::string chooseNextWord(prefix pref, std::mt19937 gen);
-private: 
-    std::vector<std::string> readWords(std::string Filepath);
-    std::map < prefix, std::vector<std::string>> stateTab;
-    prefix start_prefix;
-    prefix end_prefix;
+ public:
+  explicit TextGeneratorClass(std::string Filepath);
+  std::string generateText();
+  std::map < prefix, std::vector<std::string>> buildStateTab
+  (std::vector<std::string> words);
+  prefix GetStartPrefix();
+  void SetStartPrefix(prefix newStartPrefix);
+  std::map < prefix, std::vector<std::string>> GetStateTab();
+  void SetStateTab(std::map < prefix, std::vector<std::string>> newStateTab);
+  std::string chooseNextWord(prefix pref, std::mt19937 gen);
+ private:
+  std::vector<std::string> readWords(std::string Filepath);
+  std::map < prefix, std::vector<std::string>> stateTab;
+  prefix start_prefix;
+  prefix end_prefix;
 };
 
 #endif  // INCLUDE_TEXTGEN_H_
