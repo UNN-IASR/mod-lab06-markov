cb03d61764585eaa4e50872480527c813c9c6126
Tulen4ick <zaharsazanov2015@gmail.com>
2025-04-30 14:00:03 +0300

    changed the file layout according to gtests

--- include/textgen.h
+++ include/textgen.h
@@ -1,6 +1,6 @@
 // Copyright 2022 UNN-IASR
-#ifndef INCLUDE_TEXTGENERATOR_H_
-#define INCLUDE_TEXTGENERATOR_H_
+#ifndef INCLUDE_TEXTGEN_H_
+#define INCLUDE_TEXTGEN_H_
 #include <string>
 #include <map>
 #include <vector>
@@ -8,30 +8,29 @@
 #include <iostream>
 #include <random>
 #include <deque>
-#include <random>
+//#include <random>
 
 const int NPREF = 2;
 const int MAXGEN = 1000;
 
 typedef std::deque<std::string> prefix;
 
-class TextGeneratorClass
-{
-public:
-    TextGeneratorClass(std::string Filepath);
+class TextGeneratorClass {
+public: 
+    explicit TextGeneratorClass(std::string Filepath);
     std::string generateText();
-    std::map < prefix, std::vector<std::string>> buildStateTab(std::vector<std::string> words);
+    std::map < prefix, std::vector<std::string>> buildStateTab
+    (std::vector<std::string> words);
     prefix GetStartPrefix();
     void SetStartPrefix(prefix newStartPrefix);
     std::map < prefix, std::vector<std::string>> GetStateTab();
     void SetStateTab(std::map < prefix, std::vector<std::string>> newStateTab);
     std::string chooseNextWord(prefix pref, std::mt19937 gen);
-
-private:
+private: 
     std::vector<std::string> readWords(std::string Filepath);
     std::map < prefix, std::vector<std::string>> stateTab;
     prefix start_prefix;
     prefix end_prefix;
 };
 
-#endif
+#endif  // INCLUDE_TEXTGEN_H_
--- src/textgen.cpp
+++ src/textgen.cpp
@@ -1,57 +1,52 @@
 // Copyright 2022 UNN-IASR
 #include "textgen.h"
+#include <map>
+#include <string>
+#include <vector>
 
-TextGeneratorClass::TextGeneratorClass(std::string Filepath)
-{
+TextGeneratorClass::TextGeneratorClass(std::string Filepath) {
     std::vector<std::string> words = readWords(Filepath);
     stateTab = buildStateTab(words);
 }
 
-prefix TextGeneratorClass::GetStartPrefix()
-{
+prefix TextGeneratorClass::GetStartPrefix() {
     return start_prefix;
 }
 
-void TextGeneratorClass::SetStartPrefix(prefix newStartPrefix)
-{
+void TextGeneratorClass::SetStartPrefix(prefix newStartPrefix) {
     start_prefix = newStartPrefix;
 }
 
-std::map < prefix, std::vector<std::string>> TextGeneratorClass::GetStateTab()
-{
+std::map < prefix, std::vector<std::string>> TextGeneratorClass::GetStateTab() {
     return stateTab;
 }
 
-void TextGeneratorClass::SetStateTab(std::map < prefix, std::vector<std::string>> newStateTab)
-{
+void TextGeneratorClass::SetStateTab
+(std::map < prefix, std::vector<std::string>> newStateTab) {
     stateTab = newStateTab;
 }
 
-std::vector<std::string> TextGeneratorClass::readWords(std::string Filepath)
-{
+std::vector<std::string> TextGeneratorClass::readWords(std::string Filepath) {
     std::vector<std::string> words;
     std::string word;
     std::ifstream file(Filepath);
-    if (!file)
-    {
+    if (!file) {
         return words;
     }
-    while (file >> word)
-    {
+    while (file >> word) {
         words.push_back(word);
     }
     return words;
 }
 
-std::map<prefix, std::vector<std::string>> TextGeneratorClass::buildStateTab(std::vector<std::string> words)
-{
+std::map<prefix, std::vector<std::string>> 
+TextGeneratorClass::buildStateTab(std::vector<std::string> words) {
     std::map<prefix, std::vector<std::string>> newStateTab;
     if (words.size() < NPREF)
         return newStateTab;
     prefix curPrefix(words.begin(), words.begin() + NPREF);
     start_prefix = curPrefix;
-    for (int i = NPREF; i < words.size(); ++i)
-    {
+    for (int i = NPREF; i < words.size(); ++i) {
         newStateTab[curPrefix].push_back(words[i]);
         curPrefix.pop_front();
         curPrefix.push_back(words[i]);
@@ -60,21 +55,18 @@ std::map<prefix, std::vector<std::string>> TextGeneratorClass::buildStateTab(std
     return newStateTab;
 }
 
-std::string TextGeneratorClass::generateText()
-{
+std::string TextGeneratorClass::generateText() {
     std::string genText = "";
     if (stateTab.empty())
         return genText;
     prefix curPrefix = start_prefix;
-    for (int i = 0; i < curPrefix.size(); ++i)
-    {
+    for (int i = 0; i < curPrefix.size(); ++i) {
         genText += curPrefix[i] + " ";
     }
     std::random_device rd;
     std::mt19937 gen(rd());
-    for (int count = NPREF; count < MAXGEN; ++count)
-    {
-        if(curPrefix == end_prefix)
+    for (int count = NPREF; count < MAXGEN; ++count) {
+        if (curPrefix == end_prefix)
             return genText;
         std::string nextWord = chooseNextWord(curPrefix, gen);
         genText += nextWord + " ";
@@ -86,8 +78,7 @@ std::string TextGeneratorClass::generateText()
     return genText;
 }
 
-std::string TextGeneratorClass::chooseNextWord(prefix pref, std::mt19937 gen)
-{
+std::string TextGeneratorClass::chooseNextWord(prefix pref, std::mt19937 gen) {
     auto it = stateTab.find(pref);
     if (it->second.empty())
         return "";
