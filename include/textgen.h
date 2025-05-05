93f119f7c54dba1239d88cfeebf9b35f656f6d27
LebedevaLA <148869645+LebedevaLA@users.noreply.github.com>
2025-05-05 19:17:35 +0300

    Update textgen.h cpplint2

--- include/textgen.h
+++ include/textgen.h
@@ -1,6 +1,6 @@
 // Copyright 2022 UNN-IASR
-#ifndef MOD_LAB06_MARKOV_INCLUDE_TEXTGEN_H_
-#define MOD_LAB06_MARKOV_INCLUDE_TEXTGEN_H_
+#ifndef INCLUDE_TEXTGEN_H_
+#define INCLUDE_TEXTGEN_H_
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -11,25 +11,25 @@
 #include <cstdlib>
 #include <ctime>
 #include <string>
-using namespace std;
+
 
 class Generator {
  public:
-    typedef deque<string> prefix;
-    map<prefix, vector<string>> &getStateTable() { return statetab; }
-    map<prefix, set<string>> &getPrefixSet() { return prefixSet; }
-    string result;
-    string getres() { return result; }
+    typedef std:deque<std:string> prefix;
+    map<prefix, vector<std:string>> &getStateTable() { return statetab; }
+    map<prefix, set<std::string>> &getPrefixSet() { return prefixSet; }
+    std:string result;
+    std:string getres() { return result; }
     int getNPref() const { return NPREF; }
     int getMaxGen() const { return MAXGEN; }
     explicit Generator() = default;
-    explicit Generator(string testtext);
+    explicit Generator(std:string testtext);
     void CreateText();
  private:
-    map<prefix, vector<string>> statetab;
-    map<prefix, set<string>> prefixSet;
+    std:map<prefix, std:vector<std:string>> statetab;
+    std:map<prefix, std:set<std:string>> prefixSet;
     const int MAXGEN = 1000;
     const int NPREF = 2;
     void AnalisText();
 };
-#endif  // MOD_LAB06_MARKOV_INCLUDE_TEXTGEN_H_
+#endif  // INCLUDE_TEXTGEN_H_
