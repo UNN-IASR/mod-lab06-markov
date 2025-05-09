9f0f3e213be3ebb0abc1f4c1e36fae69010daa58
plotnikovkirill <plotnikov.kirillka2005@yandex.ru>
2025-05-09 20:41:31 +0300

    add test

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,139 +1,152 @@
 // Copyright 2021 GHA Test Team
+
 #include <gtest/gtest.h>
-#include "../include/textgen.h"
-#include <sstream>
-#include <fstream>
-#include <iterator>
+
 #include <cstdlib>
 
-TEST(TextGeneratorTest, PrefixFormation) {
-    TextGenerator generator;
-    std::istringstream input("one two three four");
-    generator.build(input);
+#include <algorithm>
+#include <fstream>
+#include <iterator>
+#include <sstream>
+#include <stdexcept>
+#include <string>
 
-    ASSERT_EQ(generator.initialPrefix.size(), TextGenerator::NPREF);
+#include "textgen.h"
+
+using std::istream_iterator;
+using std::istringstream;
+using std::ifstream;
+using std::remove;
+using std::runtime_error;
+using std::string;
+
+TEST(TextGeneratorTest, PrefixFormation) {
+  TextGenerator generator;
+  istringstream input("one two three four");
+  generator.build(input);
+
+  ASSERT_EQ(generator.initialPrefix.size(), TextGenerator::NPREF);
 }
 
 TEST(TextGeneratorTest, PrefixSuffixEntrySingle) {
-    TextGenerator generator;
-    std::istringstream input("one two three");
-    generator.build(input);
+  TextGenerator generator;
+  istringstream input("one two three");
+  generator.build(input);
 
-    TextGenerator::prefix p = { "one", "two" };
-    ASSERT_EQ(generator.statetab[p].size(), 1);
-    ASSERT_EQ(generator.statetab[p][0], "three");
+  TextGenerator::prefix p = {"one", "two"};
+  ASSERT_EQ(generator.statetab[p].size(), 1);
+  ASSERT_EQ(generator.statetab[p][0], "three");
 }
 
 TEST(TextGeneratorTest, PrefixSuffixEntryMultiple) {
-    TextGenerator generator;
-    std::istringstream input("one two three one two four one two five");
-    generator.build(input);
+  TextGenerator generator;
+  istringstream input("one two three one two four one two five");
+  generator.build(input);
 
-    TextGenerator::prefix p = { "one", "two" };
-    ASSERT_EQ(generator.statetab[p].size(), 3);
+  TextGenerator::prefix p = {"one", "two"};
+  ASSERT_EQ(generator.statetab[p].size(), 3);
 }
 
 TEST(TextGeneratorTest, SingleSuffixSelection) {
-    TextGenerator generator;
-    std::istringstream input("one two three");
-    generator.build(input);
+  TextGenerator generator;
+  istringstream input("one two three");
+  generator.build(input);
 
-    TextGenerator::prefix p = { "one", "two" };
-    std::string next = generator.statetab[p][0];
-    ASSERT_EQ(next, "three");
+  TextGenerator::prefix p = {"one", "two"};
+  string next = generator.statetab[p][0];
+  ASSERT_EQ(next, "three");
 }
 
 TEST(TextGeneratorTest, MultipleSuffixSelection) {
-    TextGenerator generator;
-    std::istringstream input("one two three one two four one two five");
-    generator.build(input);
+  TextGenerator generator;
+  istringstream input("one two three one two four one two five");
+  generator.build(input);
 
-    srand(0);
-    TextGenerator::prefix p = { "one", "two" };
-    size_t index = rand() % generator.statetab[p].size();
-    ASSERT_TRUE(index >= 0 && index < generator.statetab[p].size());
+  srand(0);
+  TextGenerator::prefix p = {"one", "two"};
+  unsigned int seed = time(nullptr);
+  size_t index = rand_r(&seed) % generator.statetab[p].size();
+  ASSERT_TRUE(index >= 0 && index < generator.statetab[p].size());
 }
 
 TEST(TextGeneratorTest, TextGenerationLength) {
-    TextGenerator generator;
-    std::istringstream input("one two three four five six seven eight nine ten");
-    generator.build(input);
+  TextGenerator generator;
+  istringstream input("one two three four five six seven eight nine ten");
+  generator.build(input);
 
-    const std::string testFile = "test_output.txt";
-    generator.generate(5, testFile);
+  const string testFile = "test_output.txt";
+  generator.generate(5, testFile);
 
-    std::ifstream in(testFile);
-    std::string generatedText;
-    std::getline(in, generatedText);
+  ifstream in(testFile);
+  string generatedText;
+  getline(in, generatedText);
 
-    std::istringstream iss(generatedText);
-    int wordCount = std::distance(std::istream_iterator<std::string>(iss),
-        std::istream_iterator<std::string>());
+  istringstream iss(generatedText);
+  int wordCount = distance(istream_iterator<string>(iss),
+                           istream_iterator<string>());
 
-    ASSERT_EQ(wordCount, 5 + TextGenerator::NPREF);
-    std::remove(testFile.c_str());
+  ASSERT_EQ(wordCount, 5 + TextGenerator::NPREF);
+  remove(testFile.c_str());
 }
 
 TEST(TextGeneratorTest, EmptyInputFile) {
-    TextGenerator generator;
-    std::istringstream input("");
+  TextGenerator generator;
+  istringstream input("");
 
-    EXPECT_THROW(generator.build(input), std::runtime_error);
+  EXPECT_THROW(generator.build(input), runtime_error);
 }
 
 TEST(TextGeneratorTest, PunctuationHandling) {
-    TextGenerator generator;
-    std::istringstream input("Hello, world! How are you?");
-    generator.build(input);
+  TextGenerator generator;
+  istringstream input("Hello, world! How are you?");
+  generator.build(input);
 
-    TextGenerator::prefix p = { "Hello,", "world!" };
-    ASSERT_EQ(generator.statetab[p][0], "How");
+  TextGenerator::prefix p = {"Hello,", "world!"};
+  ASSERT_EQ(generator.statetab[p][0], "How");
 }
 
 TEST(TextGeneratorTest, RepeatedSequences) {
-    TextGenerator generator;
-    std::istringstream input("a b c a b d a b e");
-    generator.build(input);
+  TextGenerator generator;
+  istringstream input("a b c a b d a b e");
+  generator.build(input);
 
-    TextGenerator::prefix p = { "a", "b" };
-    ASSERT_EQ(generator.statetab[p].size(), 3);
-}
-
-TEST(TextGeneratorTest, InsufficientDataGeneration) {
-    TextGenerator generator;
-    std::istringstream input("only two words");
-    generator.build(input);
-
-    testing::internal::CaptureStderr();
-    generator.generate(100);
-    std::string output = testing::internal::GetCapturedStderr();
-
-    ASSERT_TRUE(output.find("no data available") != std::string::npos);
+  TextGenerator::prefix p = {"a", "b"};
+  ASSERT_EQ(generator.statetab[p].size(), 3);
 }
 
 TEST(TextGeneratorTest, FileOutputTest) {
-    TextGenerator generator;
-    std::istringstream input("this is a test for file output");
-    generator.build(input);
+  TextGenerator generator;
+  istringstream input("this is a test for file output");
+  generator.build(input);
 
-    const std::string testFile = "test_output.txt";
-    generator.generate(5, testFile);
+  const string testFile = "test_output.txt";
+  generator.generate(5, testFile);
 
-    std::ifstream in(testFile);
-    ASSERT_TRUE(in.good());
+  ifstream in(testFile);
+  ASSERT_TRUE(in.good());
 
-    std::string content;
-    std::getline(in, content);
-    ASSERT_FALSE(content.empty());
+  string content;
+  getline(in, content);
+  ASSERT_FALSE(content.empty());
 
-    std::remove(testFile.c_str());
+  remove(testFile.c_str());
 }
 
-TEST(TextGeneratorTest, LongWordHandling) {
-    TextGenerator generator;
-    std::istringstream input("ThisIsAVeryLongWordWithNoSpaces AnotherLongWord");
-    generator.build(input);
+TEST(TextGeneratorTest, SpecialCharactersHandling) {
+  TextGenerator generator;
+  istringstream input("word1 $%^ word2 @#$ word3");
+  generator.build(input);
+  
+  TextGenerator::prefix p = {"word1", "$%^"};
+  ASSERT_EQ(generator.statetab[p].size(), 1);
+  ASSERT_EQ(generator.statetab[p][0], "word2");
+}
 
-    TextGenerator::prefix p = { "ThisIsAVeryLongWordWithNoSpaces", "AnotherLongWord" };
-    ASSERT_TRUE(generator.statetab.find(p) != generator.statetab.end());
+TEST(TextGeneratorTest, RepeatedWordsHandling) {
+  TextGenerator generator;
+  istringstream input("word word word word word");
+  generator.build(input);
+  
+  TextGenerator::prefix p = {"word", "word"};
+  ASSERT_GE(generator.statetab[p].size(), 1);
 }
