eeb604c923792a88859c3265976b0977698a225d
Kolobok <kolobkow.nikita@yandex.ru>
2025-05-25 18:34:35 +0300

    add textgen and tests

---
+++ include/textgen.h
@@ -0,0 +1,39 @@
+// Copyright 2022 UNN-IASR
+#ifndef INCLUDE_TEXTGEN_H_
+#define INCLUDE_TEXTGEN_H_
+#include <deque>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <random>
+#include <string>
+#include <vector>
+
+typedef std::deque<std::string> prefix;  // очередь префиксов
+
+class TextGenerator {
+public:
+    TextGenerator(std::string FilePath, int prefixLen) {
+        prefixLen_ = prefixLen;
+        analyzeText(FilePath);
+    }
+    explicit TextGenerator(int prefixLen) { prefixLen_ = prefixLen; }
+
+    std::string genText(int testLen);
+    std::string genSuffix(const prefix& pref);
+
+    void addTransition(
+        const prefix& pref,
+        const std::string& word);
+
+    prefix getEnd() { return endPref_; }
+
+private:
+    void analyzeText(const std::string& FilePath);
+
+    int prefixLen_;
+    prefix initPref_;
+    prefix endPref_;
+    std::map<prefix, std::vector<std::string> > statetab_;  // префикс-суффиксы
+};
+#endif  // INCLUDE_TEXTGEN_H_
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+// Copyright 2022 UNN-IASR
+#include <iostream>
+#include <string>
+
+#include "textgen.h"
+
+int main() {
+	TextGenerator tg("data.txt", 2);
+	std::ofstream file("gen.txt");
+	std::string text = tg.genText(1000);
+	file << text;
+}
