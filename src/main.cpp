a6a722fda626ad85c3306a1f9370a92c44d09a31
Vladislav Melnikov <ninthofhokage@gmail.com>
2025-06-06 00:05:23 +0300

    Fixed main

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+
+#include "textgen.h"
+#include <fstream>
+#include <sstream>
+
+int main() {
+
+    const int NPREF = 2;
+    const int MAXGEN = 1000;
+
+    TextGenerator generator(NPREF);
+
+    std::ifstream file("input_text.txt");
+    generator.create_suffix_map(file);
+
+    auto map = generator.get_suffix_map();
+
+    std::string result = generator.generate(MAXGEN);
+
+    std::ofstream out("generated.txt");
+    out << result << std::endl;
+}
