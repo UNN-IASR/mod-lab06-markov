bb98eb23232be71457021e6bea1ed57247b1b0b5
A-Sabu <a.saburova04@mail.ru>
2025-06-07 12:52:30 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,9 @@
 #include "textgen.h"
 
 int main(int argc, char* argv[]) {
-    SetConsoleOutputCP(CP_UTF8);
     Markov generator;
     generator.learnFromFile("input.txt");
     generator.generateText();
     generator.recordGeneratedTextToFile("gen.txt");
-    std::cout << generator.getGeneratedText() << std::endl;
     return 0;
 }
