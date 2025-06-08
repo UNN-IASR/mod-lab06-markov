ed9a7aac8f1eb7d852cb063b4987ed0ea72e1ed9
maxg322 <smaxim152@mail.ru>
2025-06-08 13:18:16 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,14 @@
-// Copyright 2024 StatsenkoArs
-
-#include <string>
-#include <fstream>
-#include "textgen.h"
-
+// Copyright 2024 Korshunov Yuriy
+#include "../include/textgen.h"
 
 int main() {
-    std::string line;
-    std::string text = "";
-    std::ifstream in("input.txt");
-    if (in.is_open()) {
-        while (std::getline(in, line)) {
-            text += line;
-        }
-    }
-    in.close();
+    std::ifstream file("input.txt");
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    std::string text = buffer.str();
 
-    Markov chain;
-    std::string gen_text = chain.gen(text);
-
-    std::ofstream out("output.txt");
-    if (out.is_open()) {
-        out << gen_text;
-    }
-
-    return 0;
+    Generator generator;
+    generator.Train(text, 2);
+    std::string generated_text = generator.Generate(1000);
+    std::cout << generated_text;
 }
