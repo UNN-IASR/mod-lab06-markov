8fe440a0cda71eb58a4082b513a625eea48af512
maxg322 <smaxim152@mail.ru>
2025-06-08 13:20:27 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,65 +1,45 @@
+// Copyright 2024 Korshunov Yuriy
+#include <gtest/gtest.h>
+#include "../include/textgen.h"
 
-// Copyright 2024 StatsenkoArs
-
-#include <gtest/gtest.h>  
-#include <vector>        
-#include <string>         
-
-#include "textgen.h"       
-
-
-TEST(gen_prefix, test1) {
-    Markov chain;
-    std::string in = "The difference between stupidity"
-        " and genius is that genius has its limits.";
-    std::vector<std::string> words = chain.mySplit(in, ' ');
-    int len = 3;
-    int pos = 4;
-    prefix pre = chain.gen_pre(words, pos, len);
-    for (int i = 0; i < len; i++)
-        EXPECT_EQ(words[pos - len + i], pre.at(i));
+TEST(test1, PrefixFormation) {
+    Generator generator;
+    generator.Train("the quick brown", 2);
+    Prefix prefix = {"the", "quick"};
+    std::map<Prefix, std::vector<std::string>> ch = generator.getChain();
+    EXPECT_EQ(ch.begin()->first, prefix);
 }
 
-TEST(gen_prefix_suffix, test2) {
-    Markov chain;
-    std::string in = "The difference between stupidity"
-        " and genius is that genius has its limits.";
-    chain.gen_tab(in, 11);
-    prefix key({ "The", "difference", "between", "stupidity",
-        "and", "genius", "is", "that", "genius", "has", "its" });
-    std::vector<std::string> value{ "limits." };
-    for (const auto& el : chain.statetab) {
-        EXPECT_EQ(key, el.first);
-        EXPECT_EQ(value, el.second);
-    }
+TEST(test2, CheckSuffix) {
+    Generator generator;
+    generator.Train("the quick brown fox jumps over the lazy dog", 2);
+    Prefix prefix = {"the", "quick"};
+    EXPECT_EQ(generator.getChain().at(prefix)[0], "brown");
 }
 
-TEST(pick_suffix, test3) {
-    Markov chain;
-    std::string in = "The difference between";
-    chain.gen_tab(in, 2);
-    prefix pre({ "The", "difference" });
-    std::string text = chain.pick_next(pre);
-    ASSERT_TRUE((text == "between") || (text == "burgers"));
+TEST(test3, SingleSuffixSelection) {
+    Generator generator;
+    generator.Train("the quick brown fox jumps over the lazy dog", 2);
+    std::string gen = generator.Generate(100, 10);
+    EXPECT_EQ(gen, "the lazy dog ");
 }
 
-TEST(pick_single_suffix, test4) {
-    Markov chain;
-    std::string in = "The difference between The difference burgers";
-    chain.gen_tab(in, 2);
-    prefix pre({ "The", "difference" });
-    std::string text = chain.pick_next(pre);
-    ASSERT_TRUE((text == "between") || (text == "burgers"));
+TEST(test4, MultiplySuffixSelection) {
+    Generator generator;
+    generator.Train("the quick brown fox quick jumps over the lazy dog", 1);
+    Prefix prefix = {"quick"};
+    std::vector<std::string> suffix = { "brown", "jumps" };
+    EXPECT_EQ(suffix, generator.getChain().at(prefix));
 }
 
-TEST(gen_all_text, test5) {
-    Markov chain;
-    chain.statetab = {
-        { { "a", "b" }, { "c", "d" } },
-        { { "1", "2" }, { "3" }}
+
+TEST(test5, TextFormation) {
+    Generator generator;
+    std::map<Prefix, std::vector<std::string> > chain = {
+        { { "the" }, { "quick", "brown" } },
+        { { "fox" }, { "quick" } }
     };
-    std::string text = chain.gen_text(3);
-    ASSERT_TRUE((text == "1 2 3 ")
-        || (text == "a b c ")
-        || (text == "a b d "));
+    generator.setChain(chain);
+    std::string res = generator.Generate(10, 20);
+    EXPECT_EQ("fox quick ", res);
 }
