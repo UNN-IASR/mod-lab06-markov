1a709816febc907ec1baab01a444f9328e8759a1
ArriVeder4i <79801297+ArriVeder4i@users.noreply.github.com>
2025-06-08 12:16:00 +0300

    Update textgen.h

--- include/textgen.h
+++ include/textgen.h
@@ -12,9 +12,15 @@ typedef std::deque<std::string> prefix;
 typedef std::map<prefix, std::vector<std::string>> statetab_t;
 
 // Builds the prefix-to-suffix map from the input stream.
-void build_prefix_map(std::istream& in, statetab_t& statetab, int npref);
+void build_prefix_map(
+    std::istream &in,
+    statetab_t &statetab,
+    int npref);
 
 // Generates up to maxgen words based on the state table.
-std::vector<std::string> generate_text(const statetab_t& statetab, int npref, int maxgen);
+std::vector<std::string> generate_text(
+    const statetab_t &statetab,
+    int npref,
+    int maxgen);
 
 #endif  // INCLUDE_TEXTGEN_H_
